b74f9b0e0cae252607276c7458bfc081991fd273
will-lima22 <96876300+will-lima22@users.noreply.github.com>
2026-02-11 20:38:54 -0300

    test: adiciona secret fake para validar Gitleaks + DefectDojo

--- secrets.py
+++ secrets.py
@@ -3,6 +3,7 @@
 API_KEY = "12345-ABCDE-SUPER-SECRET"
 DB_PASSWORD = "Password123!"
 TOKEN = "ghp_exampleSuperToken123456X"
+# teste lab - secret fake para disparar gitleaks
 SECRET_KEY = "sk_live_abcdefghijklmnopqrstuvxz"
 AWS_ACCESS_KEY_ID = "AKIAIOSFODNN7EXAMPLE"
 AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
