d493d1b17e10abc8397729c0eaec62dc9aa76fa2
will-lima22 <96876300+will-lima22@users.noreply.github.com>
2026-02-12 14:20:55 -0300

    fix: resolve conflicts in secrets.py for TruffleHog

--- secrets.py
+++ secrets.py
@@ -1,20 +1,6 @@
-# só adicionando um comentário
-
-<<<<<<< HEAD
-API_KEY = "12345-ABCDE-SUPER-SRET"
-=======
 API_KEY = "12345-ABCDE-SP-SECRET"
->>>>>>> 32dce6a (test: new alteration for test)
 DB_PASSWORD = "Password123!"
 TOKEN = "ghp_exampleSuperToke23456X"
-# teste lab - secret fake para disparar gitleaks
-<<<<<<< HEAD
-SECRET_KEY = "sk_live_abcdefghjklxz"
-AWS_ACCESS_KEY_ID = "AKIAIOSFODN7EXAMPLE"
-# Change for tests 4
-AWS_SECRET_ACCESS_KEY="FAKE-TEST-KEY-1236789"
-=======
 SECRET_KEY = "sk_live_abcdefghijxz"
 AWS_ACCESS_KEY_ID = "AKIAIOSFON7EXAMPLE"
 AWS_SECRET_ACCESS_KEY = "wJXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
->>>>>>> 32dce6a (test: new alteration for test)
