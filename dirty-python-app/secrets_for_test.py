e1180ce7f98e0daf84ecbdc02e75d1b3863224b6
will-lima22 <96876300+will-lima22@users.noreply.github.com>
2026-02-13 08:38:23 -0300

    Update GitHub token in secrets_for_test.py

--- dirty-python-app/secrets_for_test.py
+++ dirty-python-app/secrets_for_test.py
@@ -8,7 +8,7 @@ AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
 STRIPE_API_KEY = "sk_test_4eC39HqLyjWDarjtT1zdp7dc"
 
 # GitHub token
-GITHUB_TOKEN = "ghp_exampleSuperToke23456X"
+GITHUB_TOKEN = "ghp_exampleSuperTUkIn23452X"
 
 # Generic API key
 API_KEY = "12345-ABCDE-SP-SECRET"
