95b4ffb1a00869648ce755f6c003c72c086f76de
will-lima22 <96876300+will-lima22@users.noreply.github.com>
2026-02-13 16:09:16 -0300

    ci: add TruffleHog secret scan (monitoring only)

--- dirty-python-app/secrets_for_test.py
+++ dirty-python-app/secrets_for_test.py
@@ -1,4 +1,4 @@
 # dirty-python-app/secrets_for_test.py
-# FAKE GitHub PAT – usado apenas para teste de detecção (TruffleHog)
+# FAKE GitHub PAT 2 – usado apenas para teste de detecção (TruffleHog)
 
-GITHUB_TOKEN = "github_pat_1AbC_dE9FghI_JkLmN0pQrST_uvWXyZ12_34"
+GITHUB_TOKEN = "github_pat_2BcD_eF8FhIj_JkLmN0pQrST_uvWXyZ12_34"
