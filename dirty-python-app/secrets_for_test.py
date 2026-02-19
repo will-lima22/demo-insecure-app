27afec6820ab72f5c9e1032ceb2fbe0de3b54bdc
will-lima22 <96876300+will-lima22@users.noreply.github.com>
2026-02-19 10:45:52 -0300

    test: new secret fake (monitoring)

--- dirty-python-app/secrets_for_test.py
+++ dirty-python-app/secrets_for_test.py
@@ -1,4 +1,4 @@
-# dirty-python-app/secrets_for_test.py
-# FAKE GitHub PAT 3 – usado apenas para teste de detecção (TruffleHog)
-
-GITHUB_TOKEN = "github_pat_1DEz_eF8FhIj_JkLmN0pQrST_tyWXyZ89_12"
+	# dirty-python-app/secrets_for_test.py
+	# FAKE GitHub PAT – usado apenas para teste de detecção (TruffleHog)
+	
+GITHUB_TOKEN = "github_pat_1AbC_dE9FghI_JkLmN0pQrST_uvWXyZ12_34"
