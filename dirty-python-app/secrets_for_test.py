433f0dbb91f7ab916122717b609e0278124f27e9
will-lima22 <96876300+will-lima22@users.noreply.github.com>
2026-02-19 11:22:41 -0300

    test: new fake github token for secret scan

--- dirty-python-app/secrets_for_test.py
+++ dirty-python-app/secrets_for_test.py
@@ -1,4 +1,4 @@
 # dirty-python-app/secrets_for_test.py
 # FAKE GitHub PAT – apenas para teste de detecção (TruffleHog)
 
-GITHUB_TOKEN = "github_pat_ABC123xyz789DEF456ghiJKL012mnoPQRstu"
+GITHUB_TOKEN = "github_pat_BCD234xyz789DEF456ghiJKL012mnoPQRrto"
